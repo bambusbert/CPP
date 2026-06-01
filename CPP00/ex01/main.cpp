06d579d0010a7269eb03451b334d8482fc8b0cb4
Stefan Lambert <slambert@student.42vienna.com>
2026-06-01 16:35:42 +0200

    implemented ctrl+d logic

--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -6,15 +6,16 @@
 /*   By: slambert <slambert@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/31 12:30:57 by slambert          #+#    #+#             */
-/*   Updated: 2026/06/01 16:18:09 by slambert         ###   ########.fr       */
+/*   Updated: 2026/06/01 16:33:23 by slambert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phonebook.hpp"
 #include <iostream>
 
-void add_contact(PhoneBook &book);
-void search_contact (PhoneBook &book);
+void say_bye();
+int add_contact(PhoneBook &book);
+int search_contact (PhoneBook &book);
 
 int main (void)
 {
@@ -25,30 +26,50 @@ int main (void)
     while (true)
     {
         std::cout << "What do you wanna do? you can ADD, SEARCH or EXIT." << std::endl;
-        std::getline(std::cin, input); //TODO protection
-        if (input == "ADD")
-            add_contact(book);
-        if (input == "SEARCH")
-            search_contact(book);
-        if (input == "EXIT")
+        if (!std::getline(std::cin, input))
+        {
+            say_bye();
             return 0;
+        }
+        if (input == "ADD" && !add_contact(book))
+        {
+            say_bye();
+            return 0;
+        }  
+        if (input == "SEARCH" && !search_contact(book))
+        {
+            say_bye();
+            return 0;
+        }
+        if (input == "EXIT")
+        {
+            say_bye();
+            return 0;
+        }
     }
     return 0;
 }
 
+void say_bye()
+{
+    std::cout << "\nExiting phonebook..." << std::endl;
+}
+
 //TODO should i additionally check if the index is a valid one?
 //e.g. only 3 contact stored and index is 6
-void search_contact (PhoneBook& book)
+int search_contact (PhoneBook& book)
 {
     std::string id;
     
     book.printAll();
     std::cout << "gimme index" << std::endl;
-    std::getline (std::cin, id);
+    if (!std::getline (std::cin, id))
+        return 0;
     if (id.length() == 1 && id[0] >= '0' && id[0] <= '7')
         book.printContact(id[0] - '0');
     else
         std::cout << "invalid index! has to be between 0 and 7" << std::endl;
+    return 1;
 }
 
 int is_empty(std::string str)
@@ -61,7 +82,7 @@ int is_empty(std::string str)
     return 0;
 }
 
-void add_contact(PhoneBook& book)
+int add_contact(PhoneBook& book)
 {
     std::string fn;
     std::string ln;
@@ -70,24 +91,30 @@ void add_contact(PhoneBook& book)
     std::string sec;
     
     std::cout << "Enter first name." << std::endl;
-    std::getline(std::cin, fn);
+    if (!std::getline(std::cin, fn))
+        return 0;
     if (is_empty(fn))
-        return;        
+        return 1;        
     std::cout << "Enter last name." << std::endl;
-    std::getline(std::cin, ln);
+    if (!std::getline(std::cin, ln))
+        return 0;
     if (is_empty(ln))
-        return;   
+        return 1;   
     std::cout << "Enter nickname." << std::endl;
-    std::getline(std::cin, nn);
+    if (!std::getline(std::cin, nn))
+        return 0;
     if (is_empty(nn))
-        return;   
+        return 1;   
     std::cout << "Enter phone number." << std::endl;
-    std::getline(std::cin, num);
+    if (!std::getline(std::cin, num))
+        return 0;
     if (is_empty(num))
-        return;   
+        return 1;   
     std::cout << "Enter darkest secret." << std::endl;
-    std::getline(std::cin, sec);
+    if (!std::getline(std::cin, sec))
+        return 0;
     if (is_empty(sec))
-        return;   
+        return 1;
     book.addContact(fn, ln, nn, num, sec);
+    return 1;
 }
